19512a7ad7bcb77585d4bdfd869042a5043a3c33
Israa Almahmoud <99071791@mydavinci.nl>
2023-04-06 01:03:39 +0200

    step 4

--- helloWorld.php
+++ helloWorld.php
@@ -1,5 +1,18 @@
 <?php
-$greeting = "Learning PHP";
+$greeting = "Hello";
+$greeting1 = "World";
+
 define("greeting","Hello World!");
-echo greeting;
+echo "<h1> $greeting  $greeting1 </h1>";
 ?>
+
+
+
+
+
+
+
+    <!-- step 2 -->
+<!-- $greeting = "Learning PHP";
+define("greeting","Hello World!");
+echo greeting; -->
