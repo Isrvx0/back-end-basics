2c48883a5b382cf750a70dddaa2020778f636b36
Israa Almahmoud <99071791@mydavinci.nl>
2023-04-06 01:11:07 +0200

    some change

--- helloWorld.php
+++ helloWorld.php
@@ -5,7 +5,6 @@ echo "<h1> $greeting[0] $greeting[1] </h1>";
 
 
 
-
     <!-- step 2 -->
 <!-- $greeting = "Learning PHP";
 define("greeting","Hello World!");
@@ -18,3 +17,9 @@ $greeting1 = "World";
 
 define("greeting","Hello World!");
 echo "<h1> $greeting  $greeting1 </h1>"; -->
+
+
+    <!-- step 5 -->
+<!-- $greeting = array("hello" , "world");
+echo "<h1> $greeting[0] $greeting[1] </h1>"; -->
+
