7355507e1f23c547b1a3cf1434d929f49e87a179
Israa Almahmoud <99071791@mydavinci.nl>
2023-04-06 01:07:16 +0200

    step 5

--- helloWorld.php
+++ helloWorld.php
@@ -1,13 +1,7 @@
 <?php
-$greeting = "Hello";
-$greeting1 = "World";
-
-define("greeting","Hello World!");
-echo "<h1> $greeting  $greeting1 </h1>";
+$greeting = array("hello" , "world");
+echo "<h1> $greeting[0] $greeting[1] </h1>";
 ?>
-
-
-
 
 
 
@@ -16,3 +10,11 @@ echo "<h1> $greeting  $greeting1 </h1>";
 <!-- $greeting = "Learning PHP";
 define("greeting","Hello World!");
 echo greeting; -->
+
+
+    <!-- step 4 -->
+<!-- $greeting = "Hello";
+$greeting1 = "World";
+
+define("greeting","Hello World!");
+echo "<h1> $greeting  $greeting1 </h1>"; -->
