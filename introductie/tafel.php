57890f3d78936d4a52e8c9548adb59012acde338
Israa Almahmoud <99071791@mydavinci.nl>
2023-04-07 13:10:36 +0200

    making the code dry

--- introductie/tafel.php
+++ introductie/tafel.php
@@ -13,8 +13,3 @@ foreach($numbers as $key) {
     multiplication_tables($key);
     echo "<br/>";
   }
-
-
-// Maak een PHP applicatie die met een for loopje de tafel van 6 laat zien.
-// 2. Zorg er nu voor dat de for loop in een functie staat die de waarde 6 mee krijgt als parameter.
-// 3. Maak een array met de waardes 3, 5, 8 & 12 en loop hier doorheen met een foreach en laat zo voor ieder cijfer de tafel zien
