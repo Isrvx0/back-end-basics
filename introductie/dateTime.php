fcede56205006a06fa75a5a7647f70dd243fa73e
Israa Almahmoud <99071791@mydavinci.nl>
2023-04-19 11:56:50 +0200

    update 'verbeteren'

--- introductie/dateTime.php
+++ introductie/dateTime.php
@@ -12,15 +12,45 @@
         }
     return $greeting;
     } 
-
+    function background_image(){
+        $timeOfDay = date("H:i:a"); 
+            if( $timeOfDay >= '06:00'and $timeOfDay < '12:00'){
+                return 'images/morning.png';
+            } else if ($timeOfDay >= '12:00'and $timeOfDay < '18:00'){
+                return 'images/afternoon.png';
+            }else if ($timeOfDay >= '18:00'and $timeOfDay < '00:00'){
+                return 'images/evening.png';
+            }else if ($timeOfDay >= '00:00'and $timeOfDay < '06:00'){
+                return 'images/night.png';
+            }
+        }
 ?>  
 <!DOCTYPE html>
 <html lang="en">
-    <?php include('style.php'); ?>
-    <body class="hello">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Date Time </title>
+        <style>
+            @import url('https://fonts.googleapis.com/css?family=Lobster');  
+            body {
+                background-image: url('<?php echo background_image(); ?>');
+                background-repeat: no-repeat;
+                background-size: 100%;
+            }
+            .centered {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                color: white;
+                font-family: Lobster;  
+            }
+        </style>
+    </head>
+    <body>
         <div class="centered">
-            <h1> <?php echo greeting_message() ?> </h1>
-        </div>
+            <h1> <?php echo greeting_message();?> </h1>
         </div>
     </body>
 </html>
