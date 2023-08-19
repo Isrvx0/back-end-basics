78b81025401a28e810120126f7af1f1993e5e4f0
Israa Almahmoud <99071791@mydavinci.nl>
2023-08-19 14:24:36 +0200

    lesson 3

---
+++ cursus/lesson3.php
@@ -0,0 +1,18 @@
+<h2>Lesson 3 ==> Operators in PHP </h2>
+<?php
+/* LESSON 3: */
+    /* Operators in PHP: */
+    # How to add text to a text :
+    $text1 = "A";
+    $text1 .= "B" ;
+    $text1 .= "C";
+    echo ($text1);
+
+    # How to add number tp numbers
+    $num = 5;
+    $num += 7;
+    $num -= 2;
+    $num *= 2;
+    $num /= 4;
+    echo ($num)."<hr>";
+?>
