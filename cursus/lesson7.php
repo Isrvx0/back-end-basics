00710441ea62bee64f453fdcd746036de42e6193
Israa Almahmoud <99071791@mydavinci.nl>
2023-08-21 16:38:37 +0200

    Lesson7 => if statement

---
+++ cursus/lesson7.php
@@ -0,0 +1,16 @@
+<?php
+
+    /* IF STATEMENT IN PHP*/
+
+    $number1 = 5;
+    $number2 = 3;
+
+    if($number1 >= $number2) {
+        echo ("I love you!");
+    } else if ($number1 == $number2) {
+        echo ("I dont know!");
+    } else {
+        echo("I hate you!");
+    }
+
+?>
