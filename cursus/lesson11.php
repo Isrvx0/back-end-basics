b7cf4371d0622e07f2e3d94a8e33638c33788e6f
Israa Almahmoud <99071791@mydavinci.nl>
2023-08-24 18:29:01 +0200

    reading files in php

---
+++ cursus/lesson11.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        h1 {
+            text-align: center;
+        }
+        div {
+            border: 5px solid #018749;
+            padding: 5px;
+            background-color: #ACE1AF;
+            align-items: center;
+        }
+        textarea {
+            width: 100%;
+            height: 150px;
+            padding: 12px 20px;
+            box-sizing: border-box;
+            border: 2px solid #ccc;
+            border-radius: 4px;
+            background-color: #f8f8f8;
+            resize: none;
+        }
+    </style>    
+    <body>
+        <?php
+            $text = "";
+
+            if ($_SERVER["REQUEST_METHOD"]=='POST') {
+                $myfile = fopen("myfile.txt",'r');
+                $text = fread($myfile,filesize("myfile.txt"));
+                fclose($myfile);
+                        # 'fopen' will open the file, then 'fread' will read the fill (print it), then we'll close it with 'fclose'.
+                        # 'filesize' returns how many characters are in the text file. 
+            }
+        ?>
+        <form method="post">
+            <div>
+                <h1> Read file form </h1>
+
+                <label> File content </label>
+                <textarea name="textfiile" placeholder="type.."><?php echo $text; ?></textarea>
+                <button>submit</button>
+            </div>  
+        </form>
+    </body>
