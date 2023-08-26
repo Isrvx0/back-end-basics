d006683e20c533f9e593ab14569e090c824ad7ff
Israa Almahmoud <99071791@mydavinci.nl>
2023-08-26 17:22:08 +0200

    inssert , update and delete in mysql table

--- cursus/lesson20.php
+++ cursus/lesson20.php
@@ -11,13 +11,23 @@
                 $user = "root";
                 $pass = "";
                 $db   = "test";
-                
+
+                    #connecten to mysql in php.
                 $connect = mysqli_connect($host, $user, $pass, $db);
-                if ($connect) {
-                    echo "Connected";
-                }else {
-                    echo "Not Connected!";
+                
+                $insert = "insert into emp values(1,'jimin','South-Korea')";
+                $update = "update emp set empname= 'Felix' where empno=1";
+                $delete = "delete from emp where empno=1";
+
+                $q= mysqli_query($connect,$delete);
+
+                if ($q){
+                    echo "OK!";
+                }else{
+                    echo mysqli_error($connect);
+
                 }
+
             }
 
 
@@ -25,7 +35,6 @@
          <form method="post"> 
             <div>
                 <h1> Test Form </h1>
-            
                 <button name="btntest"> Click To Test </button>    
             </div>
             <br>
