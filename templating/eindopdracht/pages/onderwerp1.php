984872eb5f2aa1266740c4ea8771881412cbede7
Israa <99071791@mydavinci.nl>
2024-02-29 20:17:24 +0100

    onderwewrp 1

--- templating/eindopdracht/pages/onderwerp1.php
+++ templating/eindopdracht/pages/onderwerp1.php
@@ -1,3 +1,20 @@
-<?php
-?>
-<!-- jouw HTML met de inhoud over onderwerp 1 komt hier... -->
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Onderwerp 1 - Sportpagina</title>
+</head>
+<body>
+    <?php include("../includes/header.php"); ?>
+
+    <div id="content">
+            <h1>Sport</h1>
+            <img class="image-onderwerp" src="../images/gym.jpg" alt="Onderwerp 1">
+            <p>Sport is an essential part of a healthy lifestyle, as it offers many benefits, both physically and mentally. <br>By exercising regularly, you can not only improve your physical fitness, lose some weight or build muscle, but exercise also helps reduce stress and improve your mood.<br>It is important to choose a sport that you enjoy so that you stay motivated to exercise regularly, and to believe in the idea that exercise is not just a way to lose weight or build muscle, but rather a choice for a healthy and active life.</p>
+    </div>
+
+    <?php include("../includes/footer.php"); ?>
+</body>
+</html>
