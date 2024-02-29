213e42acf050107a121b8a0688d321b2e6458a7c
Israa <99071791@mydavinci.nl>
2024-02-29 19:46:32 +0100

    files + the header

---
+++ templating/eindopdracht/includes/header.php
@@ -0,0 +1,28 @@
+<!-- jouw HTML voor een Header komt hier... 
+Gebruik hier tenminste een header afbeelding en een menu
+Zorg dat je in het menu bij elk item een url parameter zet
+om te bepalen welke inhoud er ingeladen moet worden in je html
+-->
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="../css/style.css" /> 
+
+    <header>
+        <!-- Header afbeelding -->
+        <img class="header-img" src="../images/logo.jpg" alt="Header Afbeelding">
+
+        <!-- Menu -->
+        <nav>
+            <ul>
+                <li><a href="../index.php?onderwerp=onderwerp1">Onderwerp 1</a></li>
+                <li><a href="../index.php?onderwerp=onderwerp2">Onderwerp 2</a></li>
+                <li><a href="../index.php?onderwerp=onderwerp3">Onderwerp 3</a></li>
+            </ul>
+            </ul>
+        </nav>
+    </header>
+
