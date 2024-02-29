fbd3b684e84a250e1c00fe8247ccac6aed139a2e
Israa <99071791@mydavinci.nl>
2024-02-29 20:20:56 +0100

    onderwerp 3

--- templating/eindopdracht/pages/onderwerp3.php
+++ templating/eindopdracht/pages/onderwerp3.php
@@ -1,3 +1,20 @@
-<?php
-?>
-<!-- jouw HTML met de inhoud over onderwerp 3 komt hier... -->
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Onderwerp 1 - Cooking-Baking</title>
+</head>
+<body>
+    <?php include("../includes/header.php"); ?>
+
+    <div id="content">
+            <h1>Cooking / Baking</h1>
+            <img class="image-onderwerp" src="../images/cooking.jpg" alt="Onderwerp 1">
+            <p>Cooking and baking are not only necessary skills to prepare healthy and tasty meals, but they are also creative processes that allow you to create your own culinary masterpieces.<br> From preparing simple dishes to baking complicated desserts, there is always something new to learn in the kitchen. By experimenting with different ingredients and recipes, you can improve your cooking and baking skills and develop your own unique taste.<br> Moreover, cooking and baking can be a fun and bonding activity to share with friends and family.</p>
+    </div>
+
+    <?php include("../includes/footer.php"); ?>
+</body>
+</html>
