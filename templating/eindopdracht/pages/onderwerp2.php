c3b7516de678a9d3a2ff94c6f2907bcf92868f4e
Israa <99071791@mydavinci.nl>
2024-02-29 20:23:38 +0100

    fixed some code

--- templating/eindopdracht/pages/onderwerp2.php
+++ templating/eindopdracht/pages/onderwerp2.php
@@ -4,17 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Onderwerp 1 - Drawing</title>
+    <title>Onderwerp 2 - Drawing</title>
 </head>
 <body>
-    <?php include("../includes/header.php"); ?>
 
     <div id="content">
             <h1>Drawing</h1>
             <img class="image-onderwerp" src="../images/drawing.jpg" alt="Onderwerp 1">
             <p>Drawing is a creative activity that people of all ages can enjoy. It allows you to express your imagination and create your own unique works of art. <br>Whether you like to draw realistic portraits, create abstract designs or create caricatures, drawing can be a relaxing and satisfying activity. Drawing can improve your concentration, develop your fine motor skills and refine your artistic skills. <br>It's a great way to reduce stress and relax your mind after a busy day.</p>
     </div>
-
-    <?php include("../includes/footer.php"); ?>
 </body>
 </html>
--- templating/eindopdracht/pages/onderwerp3.php
+++ templating/eindopdracht/pages/onderwerp3.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Onderwerp 1 - Cooking-Baking</title>
+    <title>Onderwerp 3 - Cooking-Baking</title>
 </head>
 <body>
-    <?php include("../includes/header.php"); ?>
 
     <div id="content">
             <h1>Cooking / Baking</h1>
@@ -15,6 +14,5 @@
             <p>Cooking and baking are not only necessary skills to prepare healthy and tasty meals, but they are also creative processes that allow you to create your own culinary masterpieces.<br> From preparing simple dishes to baking complicated desserts, there is always something new to learn in the kitchen. By experimenting with different ingredients and recipes, you can improve your cooking and baking skills and develop your own unique taste.<br> Moreover, cooking and baking can be a fun and bonding activity to share with friends and family.</p>
     </div>
 
-    <?php include("../includes/footer.php"); ?>
 </body>
 </html>
