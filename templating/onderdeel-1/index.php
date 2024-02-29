875556e7b55c1e77c12738c8e07adca0332c9f79
Israa <99071791@mydavinci.nl>
2024-02-29 21:05:24 +0100

    fixed the maps

---
+++ templating/onderdeel-1/index.php
@@ -0,0 +1,4 @@
+<?php
+
+require "variables.php";
+echo"Hello, My name is $name $surename";
--- templating/onderdeel-3/content.php
+++ templating/onderdeel-3/content.php
@@ -2,6 +2,6 @@
 $name = "Israa";
 $surename = "Almahmoud";
 
-$image = 'images/bear.jpg';
+$image = '../images/bear.jpg';
 
 $text = "Tincidunt eget nullam non nisi est. Pretium fusce id velit ut tortor pretium. Blandit massa enim nec dui nunc mattis enim ut tellus. Vitae proin sagittis nisl rhoncus mattis rhoncus. Fermentum dui faucibus in ornare quam viverra orci sagittis. Faucibus a pellentesque sit amet porttitor eget dolor morbi non. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Ipsum faucibus vitae aliquet nec ullamcorper sit amet. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Massa sapien faucibus et molestie. Et tortor consequat id porta nibh venenatis cras sed. Elementum eu facilisis sed odio morbi quis commodo odio. Sem et tortor consequat id porta. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus. Vivamus arcu felis bibendum ut tristique.";
